d89410f312cadbc78e200287b2fd94df60b433a8
Anastasia <email@example.ru>
2023-01-12 15:27:21 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import telebot
+from extentions import APIException, Convertor
+from config import TOKEN, exchanges
+import traceback
+
+exchanges = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
+TOKEN = "1596902621: AAEnMFCBOg02PDigS5fAERfgcdKYsuNwb-8"
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start','help'])
+def start(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for i in exchanges.keys():
+        text = '\n'.join((text,i))
+        bot.reply_to(message, text)
+
+@bot.message_handler(content_types=['text'])
+def converter(message: telebot.types.Message):
+    values = message.text.split(' ')
+    try:
+        if len(values) != 3:
+            raise APIException('Неверное количество параметров!')
+
+        answer = Convertor.get_price(*values)
+except APIException as e:
+    bot.reply_to(message, f"Ошибка в команде:\n{e}" )
+exception Exception as e:
+    traceback.print_tb(e.__traceback__)
+    bot.reply_to(message, f"Неизвестная ошибка:\n{e}" )
+else:
+    bot.reply_to(message, answer)
+
+bot.polling()
+
